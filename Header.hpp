d306a50979e2d8c17dd6b5240a50ce14200e6d25
Gavin A Burchett <102335590+McGavGav@users.noreply.github.com>
2023-12-06 20:18:19 -0800

    Delete Header.hpp

--- Header.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-//sfml includes + iostream
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
-#include <iostream>
-using namespace sf;
